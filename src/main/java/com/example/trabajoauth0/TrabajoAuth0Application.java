b4bf9acd84f174c15a3e7d1843c050fbf18952da
SR0jo <santiagomza12@gmail.com>
2024-05-30 10:20:29 -0300

    Initial commit

---
+++ src/main/java/com/example/trabajoauth0/TrabajoAuth0Application.java
@@ -0,0 +1,13 @@
+package com.example.trabajoauth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoAuth0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoAuth0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trabajoauth0/TrabajoAuth0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabajoauth0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoAuth0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
